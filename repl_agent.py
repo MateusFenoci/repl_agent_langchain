fddbe090572c44eabdb3171add586ba8e3ddb95f
MateusFenoci <fenocimateus@gmail.com>
2024-10-01 20:55:11 -0300

    Agente responsável por executar código em Python via PytonREPL

---
+++ repl_agent.py
@@ -0,0 +1,49 @@
+from langchain_openai import ChatOpenAI
+from langchain.agents import Tool
+from langchain.prompts import PromptTemplate
+from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain_experimental.utilities import PythonREPL
+from dotenv import load_dotenv
+import re
+import os
+
+def configure():
+    load_dotenv()
+    return os.getenv("OPENAI_API_KEY")
+
+
+model = ChatOpenAI(
+    model = "gpt-3.5-turbo",
+    api_key=configure()
+)
+
+python_repl = PythonREPL()
+
+python_repl_tool = Tool(
+    name="PythonREPL",
+    description="Um shell Python. Use isso para executar código Python. Execute apenas códigos Python seguro e válidos. " +
+                "Não execute códigos maliciosos. " +
+                "Se você precisar obter o retorno do código, use a função print().",
+    func=python_repl.run
+)
+
+agent_executor = create_python_agent(
+    llm = model,
+    tool = python_repl_tool,
+    verbose = True,
+)
+
+prompt_template = PromptTemplate(
+    input_variables = ['query'],
+    template = '''
+        Responda a problema: {query}.
+    '''
+)
+query = r"quanto é 20% de 3000"
+
+prompt = prompt_template.format(query=query)
+
+response = agent_executor.invoke(prompt)
+
+print(response.get('output'))
+
